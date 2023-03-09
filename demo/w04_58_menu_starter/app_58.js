4e35b090dc7bbff341343a0a9f934a0fb6a1590b
anan826 <david70825@gmail.com>
2023-03-09 19:14:56 +0800

    W04-P1: add 3 images of my meals to menu array

---
+++ demo/w04_58_menu_starter/app_58.js
@@ -0,0 +1,41 @@
+const menu = [
+    {
+        id: 1,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 11.59,
+        img: './images/my-1.jpeg',
+        remote_img: 'https://slyliryvslfzxeqslixp.supabase.co/storage/v1/object/public/demo-58/md_1N_img/my-1.jpg',
+        desc: 'yep, I\'m lovin\' it.'
+    },
+    {
+        id: 2,
+        title: 'lunch',
+        category: 'lunch',
+        price: 15.59,
+        img: './images/my-2.jpeg',
+        remote_img: 'https://slyliryvslfzxeqslixp.supabase.co/storage/v1/object/public/demo-58/md_1N_img/my-2.jpg',
+        desc: 'Polpette al Sugo'
+    },
+    {
+        id: 3,
+        title: 'dinner double',
+        category: 'dinner',
+        price: 18.59,
+        img: './images/my-3.jpeg',
+        remote_img: 'https://slyliryvslfzxeqslixp.supabase.co/storage/v1/object/public/demo-58/md_1N_img/my-3.jpg',
+        desc: 'absolutely is 12oz steak'
+    },
+    {
+        id: 4,
+        title: 'salad',
+        category: 'dinner',
+        price: 14.59,
+        img: './images/my-4.jpeg',
+        remote_img: 'https://slyliryvslfzxeqslixp.supabase.co/storage/v1/object/public/demo-58/md_1N_img/my-4.jpg',
+        desc: 'fresh vegetable'
+    },
+];
+
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('btn-container');
