94df6f4b349eb8b9e3d183aa9e78fe9e4e177728
anan826 <david70825@gmail.com>
2023-03-09 19:51:56 +0800

    W04-P2: show all data in array menu into html (section-center)

--- demo/w04_58_menu_starter/app_58.js
+++ demo/w04_58_menu_starter/app_58.js
@@ -1,19 +1,19 @@
 const menu = [
     {
         id: 1,
-        title: 'buttermilk pancakes',
+        title: 'McDonald\'s',
         category: 'breakfast',
         price: 11.59,
-        img: './images/my-1.jpeg',
+        img: './images/my-1.jpg',
         remote_img: 'https://slyliryvslfzxeqslixp.supabase.co/storage/v1/object/public/demo-58/md_1N_img/my-1.jpg',
         desc: 'yep, I\'m lovin\' it.'
     },
     {
         id: 2,
-        title: 'lunch',
+        title: 'Italy',
         category: 'lunch',
         price: 15.59,
-        img: './images/my-2.jpeg',
+        img: './images/my-2.jpg',
         remote_img: 'https://slyliryvslfzxeqslixp.supabase.co/storage/v1/object/public/demo-58/md_1N_img/my-2.jpg',
         desc: 'Polpette al Sugo'
     },
@@ -22,20 +22,51 @@ const menu = [
         title: 'dinner double',
         category: 'dinner',
         price: 18.59,
-        img: './images/my-3.jpeg',
+        img: './images/my-3.jpg',
         remote_img: 'https://slyliryvslfzxeqslixp.supabase.co/storage/v1/object/public/demo-58/md_1N_img/my-3.jpg',
-        desc: 'absolutely is 12oz steak'
+        desc: '12oz steak'
     },
     {
         id: 4,
         title: 'salad',
         category: 'dinner',
         price: 14.59,
-        img: './images/my-4.jpeg',
+        img: './images/my-4.jpg',
         remote_img: 'https://slyliryvslfzxeqslixp.supabase.co/storage/v1/object/public/demo-58/md_1N_img/my-4.jpg',
         desc: 'fresh vegetable'
     },
 ];
 
+const fliter = [
+    {
+        
+    },
+];
+
 const sectionCenter = document.querySelector('.section-center');
 const btnContainer = document.querySelector('btn-container');
+
+const displayMenuItems = (menu) => {
+    let displayMenu = menu.map( (item) => {
+        return `<article class="menu-item">
+        <img src="${item.img}" alt="${item.title}" class = photo" />
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">${item.price}</h4>
+          </header>
+          <p class="item-text">${item.desc}
+          </p>
+        </div>
+      </article>`
+    });
+    console.log('displayMenu before join', displayMenu);
+    displayMenu = displayMenu.join('');
+    console.log('displayMenu after join', displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    displayMenuItems(menu);
+});
+
