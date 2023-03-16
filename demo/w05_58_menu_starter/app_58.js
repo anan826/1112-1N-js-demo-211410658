c0fe3c2065dfc9506515a78a5bcdb5af2fd32a76
anan826 <david70825@gmail.com>
2023-03-16 19:43:10 +0800

    W05-P2: implement all filter buttons with all tests in console.log

--- demo/w05_58_menu_starter/app_58.js
+++ demo/w05_58_menu_starter/app_58.js
@@ -2,6 +2,8 @@ import {menu} from './data_58.js'
 
 const sectionCenter = document.querySelector('.section-center');
 const btnContainer = document.querySelector('.btn-container');
+
+
 /*menu*/
 const displayMenuItems = (menu) => {
     let displayMenu = menu.map( (item) => {
@@ -19,9 +21,9 @@ const displayMenuItems = (menu) => {
           </div>
         </article>`
     });
-    console.log('displayMenu before join', displayMenu);
+    //console.log('displayMenu before join', displayMenu);
     displayMenu = displayMenu.join('');
-    console.log('displayMenu after join', displayMenu);
+    //console.log('displayMenu after join', displayMenu);
     sectionCenter.innerHTML = displayMenu;
 };
 
@@ -42,11 +44,29 @@ const displayMenuButtons = () => {
         return `
         <button type="button" class="filter-btn" data-id="${btn}">${btn}</button>`
     });
-    console.log('displayMenuButtons before join', displayMenuButtons);
+    //console.log('displayMenuButtons before join', displayMenuButtons);
     displayMenuButtons = displayMenuButtons.join('');
-    console.log('displayMenuButtons after join', displayMenuButtons);
+    //console.log('displayMenuButtons after join', displayMenuButtons);
     btnContainer.innerHTML = displayMenuButtons;
+    const filterBtns = document.querySelectorAll('.filter-btn');
+      console.log('filter-btn', filterBtns);
+    filterBtns.forEach((btn) => {
+      btn.addEventListener('click', (e) =>{
+        console.log('data-id', e.currentTarget.dataset.id);
+        const category = e.currentTarget.dataset.id;
+        const filterMenu = menu.filter((item) => item.category === category);
+        console.log('filterMenu', filterMenu);
+        if (category === 'all'){
+          console.log('all', menu)
+          displayMenuItems(menu);
+        }else{
+          displayMenuItems(filterMenu);
+        }
+      });
+    })
 };
+
+
 
 window.addEventListener('DOMContentLoaded', () => {
     displayMenuItems(menu);
