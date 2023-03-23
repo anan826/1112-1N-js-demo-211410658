b2be949fcb527199df7bcc6cf5ccdef458486ad8
anan826 <david70825@gmail.com>
2023-03-23 19:28:52 +0800

    W06-P2: temperature convert from C to F

--- demo/w06_58/p1_58.js
+++ demo/w06_58/p1_58.js
@@ -1,5 +1,5 @@
 let c, f;
 c = Number(prompt("Enter a temperature in C:")).toFixed(2);
-f = (c * 9/5 + 20.532);
+f = (c * 9/5 + 32).toFixed(2);
 console.log(`${c} C = ${f} F`);
 alert(`${c} C = ${f} F`)
