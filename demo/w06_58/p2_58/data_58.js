bec1174bf719d83d95f6b060842b0b1a3af8677b
anan826 <david70825@gmail.com>
2023-03-23 20:31:48 +0800

    W06-P3: import students and sdata array and do sorting, find the highest and lowest score

---
+++ demo/w06_58/p2_58/data_58.js
@@ -0,0 +1,36 @@
+export const students = [
+    {
+        id: 1,
+        name: 'alpha',
+        score: 70,
+        role: 'student',
+    },
+    {
+        id: 2,
+        name: 'bravo',
+        score: 60,
+        role: 'student',
+
+    },
+    {
+        id: 3,
+        name: 'charile',
+        score: 85,
+        role: 'student',
+    },
+    {
+        id: 4,
+        name: 'delta',
+        score:55,
+        role: 'student',
+    },
+    {
+        id: 5,
+        name: 'echo',
+        score: 43,
+        role: 'student',
+    },
+    
+];
+
+export const sdata = [80,90,35,60,75,40];
---
+++ demo/w06_58/p2_58/p2_58.js
@@ -0,0 +1,26 @@
+import { students, sdata } from "./data_58.js";
+
+//for sdata
+
+console.log('sdata original', sdata);
+
+sdata.sort(function(a, b){return b-a});
+console.log('sdata sorted', sdata);
+console.log(`The hightest score: ${sdata[0]}`);
+console.log(`The lowest score: ${sdata[sdata.length-1]}`);
+//for students
+
+console.log('students', students)
+
+const students2 = students.map((student) => {
+//    student.role = 'student';
+    return {...student, role: 'student'};
+});
+
+console.log('student2 original', students2);
+
+students2.sort(function(a,b){return a.score - b.score});
+console.log('students2 sorted', students2)
+
+console.log(`The lowest score: ${students2[0].score}`);
+console.log(`The hightest score: ${students2[students2.length-1].score}`);
