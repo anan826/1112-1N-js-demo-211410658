479f2d604e6ee6ac05862701dda752d489a42feb
anan826 <david70825@gmail.com>
2023-04-13 19:37:11 +0800

    W09-P2: Midprep P2 answer

--- demo/w09_58/midprep_stud_58/p2_counter_58/p2_58.js
+++ demo/w09_58/midprep_stud_58/p2_counter_58/p2_58.js
@@ -4,6 +4,28 @@ let count = 0;
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
+console.log(`btns`, btns);
+
 btns.forEach(function (btn) {
-  btn.addEventListener('click', function (e) {});
+  btn.addEventListener('click', function (e) {
+    console.log(`e.currentTarget`, e.currentTarget.classList);
+    const styles = e.currentTarget.classList;
+    if(styles.contains('decrease')){
+      count--;
+    }else if (styles.contains('increase')){
+      count++;
+    }else{
+      count = 0;
+    }
+    if(count >0){
+      value.style.color = 'green';
+    }else if (count ===0){
+      value.style.color = 'var(--clr-grey-1);';
+    }else {
+      value.style.color = 'red';
+    }
+
+    value.textContent = count;
+  });
 });
+
