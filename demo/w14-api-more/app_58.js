0cbd7e39fde29639e52707eab341032a91ff1583
anan826 <david70825@gmail.com>
2023-05-18 20:59:59 +0800

    W14-P2: Get data from MealDB, SportDB, AudioDB one by one as in P1

--- demo/w14-api-more/app_58.js
+++ demo/w14-api-more/app_58.js
@@ -1,27 +1,111 @@
-const btn = document.querySelector('.btn');
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+const btn4 = document.querySelector('#btn4');
+const container = document.querySelector('.container')
 
-btn.addEventListener('click', async () => {
-  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+
+//cocktail db
+btn1.addEventListener('click', async () => {
+  const url_cocktail = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
   try {
-    const response = await fetch(url);
+    const response = await fetch(url_cocktail);
     const data = await response.json();
-    console.log('data', data.drinks);
-    displayItems(data.drinks);
+    console.log('data1', data.drinks);
+    displayItems_cocktail(data.drinks);
   } catch (error) {
     console.error(error);
   }
 });
 
-
-
-const displayItems = (items) => {
+const displayItems_cocktail = (items) => {
   const displayData = items
     .map((item) => {
       const { strDrink } = item;
       return `<p>${strDrink}</p>`;
     })
     .join('');
-  const element = document.createElement('div');
+  // const element = document.createElement('div');
+  const element = container
   element.innerHTML = displayData;
-  document.body.appendChild(element);
+  // document.container.appendChild(element);
 };
+//meal db
+btn2.addEventListener('click', async () => {
+  const url_meal = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+  try {
+    const response = await fetch(url_meal);
+    const data = await response.json();
+    console.log('data2', data);
+    displayItems_meal(data.meals);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const displayItems_meal = (items) => {
+  const displayData = items
+    .map((item) => {
+      const { strMeal } = item;
+      return `<p>${strMeal}</p>`;
+    })
+    .join('');
+  // const element = document.createElement('div');
+  const element = container
+  element.innerHTML = displayData;
+  // document.body.appendChild(element);
+};
+
+//sport db
+btn3.addEventListener('click', async () => {
+  const url_sports = 'https://www.thesportsdb.com/api/v1/json/3/all_leagues.php';
+  try {
+    const response = await fetch(url_sports);
+    const data = await response.json();
+    console.log('dat3', data.leagues);
+    displayItems_sports(data.leagues);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const displayItems_sports = (items) => {
+  const displayData = items
+    .map((item) => {
+      const { strLeague } = item;
+      return `<p>${strLeague}</p>`;
+    })
+    .join('');
+  // const element = document.createElement('div');
+  const element = container
+  element.innerHTML = displayData;
+  // document.body.appendChild(element);
+};
+
+//audio db
+
+btn4.addEventListener('click', async () => {
+  const url_artist = 'https://theaudiodb.com/api/v1/json/2/search.php?s=coldplay';
+  try {
+    const response = await fetch(url_artist);
+    const data = await response.json();
+    console.log('data4', data);
+    displayItems_audio(data.artists);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const displayItems_audio = (items) => {
+  const displayData = items
+    .map((item) => {
+      const { strArtist } = item;
+      return `<p>${strArtist}</p>`;
+    })
+    .join('');
+  // const element = document.createElement('div');
+  const element = container
+  element.innerHTML = displayData;
+  // document.body.appendChild(element);
+};
+
---
+++ demo/w14-api-more/p4_xx/js/app_58.js
@@ -0,0 +1,3 @@
+const btnAll = document.querySelector('#all')
+const url = `https://course-api.com/javascript-store-products`;
+
