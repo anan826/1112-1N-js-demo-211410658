e9e379083f4312aad2df2c280a5188d322b06461
anan826 <david70825@gmail.com>
2023-05-22 22:59:00 +0800

    company filter

--- demo/w14-homework_58/js/app_58.js
+++ demo/w14-homework_58/js/app_58.js
@@ -1,44 +1,80 @@
-const allBtn = document.querySelector('.all')
-const ikeaBtn = document.querySelector('.ikea')
-const marcosBtn = document.querySelector('.marcos')
-const caressaBtn = document.querySelector('.caressa')
-const liddyBtn = document.querySelector('.liddy')
+const btnAll = document.querySelectorAll('.company-btn');
+let data;
 
-const displayAPI = async ()=>{
+const fetchAPI = async ()=>{
     const url = `https://course-api.com/javascript-store-products`;
         try{
             const response = await fetch(url);
             const data = await response.json();
             console.log('data', data);
-            displayItems(data);
+            return data;
         }catch(error){
             console.log(error)
         }
 }
 
-const displayItems = (items) => {
-    const displayData = items
-      .map((item) => {
-        // const {price, image, name} = item;
-        return `<div class="single-product ${item.fields.company} all">
-        <img
-          src="${item.fields.image[0].url}"
-          class="single-product-img img"
-          alt="${item.fields.name}"
-        />
-        <footer>
-          <h5 class="name">${item.fields.name}</h5>
-          <span class="price">$${item.fields.price}</span>
-        </footer>
-      </div>`;
-      })
-      .join('');
+const displayItems = (data, textContent) => {
+  data.then((item)=>
+  {
+    const dataDisplay = item.map((item)=>
+      {
+        const {company, price, name, image} = item.fields;
+        // console.log(textContent, name);
+        if (textContent == "all" || textContent == company)
+        {
+          return `
+          <div class="single-product ${company}">
+            <img
+              src="${image[0].url}"
+              class="single-product-img img"
+              alt="${name}"
+            />
+            <footer>
+              <h5 class="name">${name}</h5>
+              <span class="price">$${price}</span>
+            </footer>
+          </div>
+          `;
+        }
+      }).join('');
+      document.querySelector('.products-container').innerHTML = dataDisplay;
+  });
+    // const displayData = items
+    //   .map((item) => {
+    //     // const {price, image, name} = item;
+    //     return `<div class="single-product ${item.fields.company} all">
+    //     <img
+    //       src="${item.fields.image[0].url}"
+    //       class="single-product-img img"
+    //       alt="${item.fields.name}"
+    //     />
+    //     <footer>
+    //       <h5 class="name">${item.fields.name}</h5>
+    //       <span class="price">$${item.fields.price}</span>
+    //     </footer>
+    //   </div>`;
+    //   })
+    //   .join('');
     // const element = document.createElement('div');
     // const element = container
-    document.querySelector('.products-container').innerHTML = displayData;
+    // document.querySelector('.products-container').innerHTML = displayData;
     // document.container.appendChild(element);
   };
   
 
-window.addEventListener('DOMContentLoaded', displayAPI);
-allBtn.addEventListener('click', displayAPI);
+window.addEventListener('DOMContentLoaded', ()=>
+{
+  data = fetchAPI();
+  displayItems(data, "company");
+});
+
+btnAll.forEach((btn)=>
+{
+  btn.addEventListener('click',(e)=>
+  {
+    if(btn.textContent == 'all')
+      displayItems(data);
+    displayItems(data, btn.textContent);
+  })
+})
+
