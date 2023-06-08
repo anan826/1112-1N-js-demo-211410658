755437cf458d07b03227ec6b5d883e120304ba15
anan826 <david70825@gmail.com>
2023-06-08 21:22:15 +0800

    P3: API Projects and DOM projects

--- demo/w17/final_1_58/p3_58/script_58.js
+++ demo/w17/final_1_58/p3_58/script_58.js
@@ -2,101 +2,111 @@ const search = document.querySelector('#search');
 const submit = document.querySelector('#submit');
 const random = document.querySelector('#random');
 const resultHeading = document.querySelector('#result-heading');
-const mealsEl = document.querySelector('#meals');
-const single_mealEl = document.querySelector('#single-meal');
+const mealsElement = document.querySelector('#meals');
+const singleMealElement = document.querySelector('#single-meal')
 
-const searchMeal = (e) => {
-  e.preventDefault();
-  const term = search.value;
-  //   console.log('term', term);
-  if (term.trim()) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('meals data', data);
 
-        if (data.meals === null) {
-          resultHeading.innerHTML = `<p> There are no search results. Try again! </p>`;
-        } else {
-          resultHeading.innerHTML = `<h2> Search Results for '${term}' </h2>`;
-          mealsEl.innerHTML = data.meals
-            .map((meal) => {
-              return `
-                  <div class="meal">
-                    <img src="${meal.strMealThumb}" />
-                    <div class="meal-info" data-mealid="${meal.idMeal}">
-                       <h3> ${meal.strMeal} </h3>
-                    </div>
-                  </div>
-                `;
-            })
-            .join('');
+const searchMeal = (e) =>{
+    e.preventDefault();
+    const term = search.value;
+    // console.log(term);
+    if(term.trim()){
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${term}`)
+        .then(response => response.json())
+        .then(data =>{
+            console.log("data", data);
+            
+            if(data.drinks === null){
+                resultHeading.innerHTML = `<p>There are no search results. Please try again later${term}</p>`
+            }else{
+                resultHeading.innerHTML = `<h2>Search Results For ${term}</h2>`
+                mealsElement.innerHTML = data.drinks.map((meal)=>{
+                    return `
+                        <div class="meal">
+                            <img src="${meal.strDrinkThumb}" />
+                            <div class="meal-info" data-mealID="${meal.idDrink}">
+                            <h3>${meal.strDrink}</h3>
+                            </div>
+                        </div>
+                        
+                    `
+                }).join('');
+            }
+        })
+
+    }else{
+        alert("Please enter a search term")
+    }
+
+}
+
+const getMealById =(mealID) =>{
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(response=>response.json())
+    .then(data=>{
+        console.log('single drink', data)
+        const meal = data.drinks[0]
+        addMealToDOM(meal)
+    })
+}
+
+const addMealToDOM = (drink)=>{
+    const ingredients =[];
+    for(let i =1;i<=20;i++){
+        if(drink[`strIngredient${i}`]){
+            ingredients.push(`${drink[`strIngredient${i}`]}-${drink[`strMeasure${i}`]}`);
+        }else{
+            break;
         }
-      });
-  } else {
-    // alert('Please enter a search term');
-  }
-};
-
-const getMealById = (mealID) => {
-  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log('single meal', data);
-      const meal = data.meals[0];
-      addMealToDOM(meal);
-    });
-};
-
-const addMealToDOM = (meal) => {
-  const ingredients = [];
-  for (let i = 1; i <= 20; i++) {
-    if (meal[`strIngredient${i}`]) {
-      ingredients.push(
-        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
-      );
-    } else {
-      break;
     }
-  }
-  console.log('ingredients', ingredients);
+    console.log(`ingredients`, ingredients)
 
-  single_mealEl.innerHTML = `
-      <div class="single-meal">
-        <h1> ${meal.strMeal} </h1>
-        <img src="${meal.strMealThumb}" />
-        <div class="main">
-           <p> ${meal.strInstructions} </p>
-           <h2> Ingredients </h2>
-           <ul>
-             ${ingredients
-               .map((ing) => {
-                 return `<li>${ing}</li>`;
-               })
-               .join('')}
-           </ul>
+    singleMealElement.innerHTML =`
+        <div class="single-meal">
+            <h1>${drink.strDrink}</h1>
+            <img src="${drink.strDrinkThumb}"/>
+            <div class="main">
+                <p>${drink.strInstructions}</p>
+                <h2>Ingredients</h2>
+                <ul> ${ingredients.map(img =>
+                    {
+                        return `<li>${img}</li>`
+                    }).join('')
+                    }
+                </ul>
+            </div>
+        
         </div>
+        `
+}
 
-      </div>
-   `;
-};
-
-// Event listeners
+const getRandomMeal = () => {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+      .then(response => response.json())
+      .then(data => {
+        console.log('random', data)
+        const randomMeal = data.drinks[0];
+        addMealToDOM(randomMeal);
+      });
+  };
+  
+//Event Listeners
 submit.addEventListener('click', searchMeal);
-
-mealsEl.addEventListener('click', (e) => {
-  // console.log('e.path', e.composedPath());
-  const composedPath = e.composedPath();
-  const mealInfo = composedPath.find((item) => {
-    if (item.classList) {
-      return item.classList.contains('meal-info');
-    } else {
-      return false;
+mealsElement.addEventListener('click', e=>{
+    console.log('e.path', e.composedPath());
+    const composedPath = e.composedPath();
+    const mealInfo = composedPath.find(item => {
+        if(item.classList){
+            return item.classList.contains('meal-info')
+        }else{
+            return false
+        }
+    })
+    if(mealInfo){
+        console.log('mealInfo', mealInfo)
+        const mealID = mealInfo.getAttribute('data-mealID')
+        getMealById(mealID);
     }
-  });
-  if (mealInfo) {
-    // console.log('mealInfo', mealInfo);
-    const mealID = mealInfo.getAttribute('data-mealid');
-    getMealById(mealID);
-  }
-});
+})
+
+random.addEventListener('click', getRandomMeal);
--- demo/w17/final_1_58/p4_58/script.js
+++ demo/w17/final_1_58/p4_58/script.js
@@ -1,58 +1,100 @@
 const main = document.querySelector('#main');
 const addUserBtn = document.querySelector('#add-user');
 const doubleBtn = document.querySelector('#double');
-const showMillionariesBtn = document.querySelector('#show-millionaires');
+const showMillionairesBtn = document.querySelector('#showMillionaires');
 const sortBtn = document.querySelector('#sort');
-const calculateBtn = document.querySelector('#calculate-wealth');
+const calculateBtn = document.querySelector("#calculate-wealth");
+function formatMoney(number) {
+
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
+}
 
 let data = [];
 
 const config = {
-  numRandom: 5,
-  ratio: 1.5,
-  showCondition: 20000000,
-};
-
-const updateDOM = (providedData = data) => {};
-
-const addData = (obj) => {
-  data.push(obj);
-  // console.log('data', data);
-  updateDOM();
-};
-
-// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
-function formatMoney(number) {
-  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    numRandom: 5,
+    ratio: 1.5,
+    showCondition: 2000000
 }
 
+const updateDOM = (providedData = data) => {
+    let tempData = providedData.map((item) => {
+        return `<div class="person"><strong>${item.name}</strong> ${formatMoney(item.money)}</div>`
+    });
+    tempData = tempData.join('');
+    console.log('tempData', tempData);
+    main.innerHTML = `<h2><strong>Person</strong> Wealth</h2> ${tempData}`
+}
+
+
+const addData = (obj) => {
+    data.push(obj);
+    console.log('data', data);
+    updateDOM();
+}
+
+
 const getRandomUser = async () => {
-  const res = await fetch('https://randomuser.me/api');
-  // add your code
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+    console.log('random user data', data)
+    const user = data.results[0];
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 10000000)
+    }
+    addData(newUser);
+}
 
-  const newUser = {
-    name: ``,
-    money: Math.floor(Math.random() * 10000000),
-  };
-  addData(newUser);
-};
+const getUser = () => {
+    for (let i = 0; i < config.numRandom; i++) {
+        getRandomUser();
+    }
+}
 
-const getUsers = () => {
-  for (let i = 0; i < config.numRandom; i++) {
-    getRandomUser();
-  }
-};
+getUser();
 
-getUsers();
-
-const changeBtnText = () => {};
+const changeBtnText = () => {
+    addUserBtn.textContent = `Add Users (${config.numRandom}) 👱‍♂️`;
+    doubleBtn.textContent = `Raise Money * ${config.ratio} 💰`;
+}
 
 changeBtnText();
 
-const sortByRichest = () => {};
+const doubleMoney = () => {
+    data = data.map((user) => {
+        return {
+            name: user.name,
+            money: user.money * 2
+        }
+    });
+    updateDOM();
+}
 
-const calculateWealth = () => {};
+
+const sortByRichest = () => {
+    data.sort((a, b) => {
+        return b.money - a.money
+    });
+
+    updateDOM();
+}
+let sum=0;
+
+const calculateWealth = () => {
+    for(let i=0;i<config.numRandom;i++){
+      const wealth = data[i].money;
+      parseInt(wealth);
+      console.log('wealth',data[i].money);
+      sum+=wealth;
+      console.log(sum);
+    }
+    
+    main.innerHTML += `<h3>Total Wealth: <strong>${formatMoney(sum)}</strong></h3>`
+}
 
 addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 calculateBtn.addEventListener('click', calculateWealth);
